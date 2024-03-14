d6048638a24f5dfff49e7aac96e781ff151b99cc
황승원 <kevin1113dev@gmail.com>
2024-03-14 20:05:53 +0900

    Create News.java

---
+++ demo/src/main/java/com/example/demo/domain/News.java
@@ -0,0 +1,11 @@
+package com.example.demo.domain;
+
+public class News {
+  private Long id;
+  private String title;
+  private String content;
+  private String author;
+  private String createdAt;
+  private String updatedAt;
+  private String link;
+}
