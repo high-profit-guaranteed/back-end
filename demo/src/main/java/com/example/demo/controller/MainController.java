2ae10b6a96719547ba9db6d7a97cab8af8e36fd2
황승원 <kevin1113dev@gmail.com>
2024-03-20 12:28:30 +0900

    로그인 수정
    로그인 세션 있을때 루트경로에 접근시 홈화면으로 redirect 하도록 수정

--- demo/src/main/java/com/example/demo/controller/MainController.java
+++ demo/src/main/java/com/example/demo/controller/MainController.java
@@ -2,15 +2,29 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
+
+import com.example.demo.domain.Member;
+import com.example.demo.service.MemberService;
 
 import ch.qos.logback.core.model.Model;
 
 @Controller
 public class MainController {
 
+  private final MemberService memberService;
+  public MainController(MemberService memberService) {
+    this.memberService = memberService;
+  }
+
   @GetMapping("/")
-  public String mainPage(Model model) {
-    return "redirect:/signin";
+  public String mainPage(Model model, @SessionAttribute(name = "id", required = false) Long id) {
+    Member member = memberService.getSigninMember(id);
+    if (member == null) {
+      return "redirect:/signin";
+    }
+
+    return "redirect:/home";
   }
 
 }
