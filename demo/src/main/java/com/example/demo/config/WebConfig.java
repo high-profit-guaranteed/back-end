87e2af773d8c1a74f365775e4a1141fde6d9be12
황승원 <kevin1113dev@gmail.com>
2024-05-07 01:58:55 +0900

    Create WebConfig.java

---
+++ demo/src/main/java/com/example/demo/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+  @Bean
+  public CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+
+    // 추가
+    configuration.addAllowedOrigin("http://duckling.d-v.kro.kr");
+    return request -> configuration;
+  }
+}
