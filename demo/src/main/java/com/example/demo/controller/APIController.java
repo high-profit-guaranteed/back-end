15072a3b00744bfd9e17ca0a185541dbe7233326
황승원 <kevin1113dev@gmail.com>
2024-05-15 15:08:11 +0900

    API 수정 및 명세서 작성

--- demo/src/main/java/com/example/demo/controller/APIController.java
+++ demo/src/main/java/com/example/demo/controller/APIController.java
@@ -3,10 +3,14 @@ package com.example.demo.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttribute;
 
@@ -15,6 +19,8 @@ import com.example.demo.domain.Member;
 import com.example.demo.service.AccountService;
 import com.example.demo.service.MemberService;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NonNull;
@@ -30,16 +36,80 @@ public class APIController {
     this.accountService = accountService;
   }
 
-  @GetMapping("/api/getAccounts")
+  @PostMapping("api/signin")
+  public ResponseEntity<String> apiSignin(@RequestBody SignIn login, HttpServletRequest httpServletRequest) {
+
+    String uid = login.getUid();
+    String password = login.getPassword();
+
+    if (uid == null || uid.isEmpty() || password == null || password.isEmpty()) {
+      return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+    }
+
+    // 멤버 존재 확인
+    Member member = memberService.signin(uid, password);
+
+    if (member == null) {
+      return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+    }
+
+    // 로그인 성공 => 세션 생성
+
+    // 세션을 생성하기 전에 기존의 세션 파기
+    httpServletRequest.getSession().invalidate();
+    HttpSession session = httpServletRequest.getSession(true); // Session이 없으면 생성
+    // 세션에 userId를 넣어줌
+    session.setAttribute("id", member.getId());
+    session.setMaxInactiveInterval(1800); // Session이 30분동안 유지
+
+    final HttpHeaders httpHeaders = new HttpHeaders();
+    httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+    return new ResponseEntity<>("Signin Success", httpHeaders, HttpStatus.OK);
+  }
+
+  @PostMapping("api/signup")
+  public ResponseEntity<String> apiSignup(@RequestBody SignUp signup, HttpServletRequest httpServletRequest) {
+
+    String uid = signup.getUid();
+    String password = signup.getPassword();
+    String name = signup.getName();
+    String email = signup.getEmail();
+
+    String emailName = email.split("@")[0];
+    String emailDomain = email.split("@")[1];
+
+    if (uid == null || uid.isEmpty() || password == null || password.isEmpty() || name == null || name.isEmpty()
+        || email == null || email.isEmpty()) {
+      return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+    }
+
+    if (emailName == null || emailDomain == null) return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+    Member member = memberService.signup(uid, password, name, emailName, emailDomain);
+    if (member == null) return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+
+    return new ResponseEntity<String>("SignUp Success", HttpStatus.OK);
+  }
+
+  @GetMapping("api/checkSession")
+  public ResponseEntity<String> postMethodName(@SessionAttribute(name = "id", required = false) Long id) {
+    if (CheckSession(id) == null) return ResponseEntity.status(HttpStatus.OK).build();
+    else return ResponseEntity.status(HttpStatus.OK).body("success");
+  }
+
+  @GetMapping("api/signout")
+  public ResponseEntity<String> signout(HttpServletRequest request) {
+
+    HttpSession session = request.getSession(false); // Session이 없으면 null return
+    if (session != null) session.invalidate(); // Session 파기
+
+    return ResponseEntity.status(HttpStatus.OK).body("Success");
+  }
+
+  @GetMapping("api/accounts")
   public ResponseEntity<Accounts> getAccounts(@SessionAttribute(name = "id", required = false) Long id) {
 
-    // check session start
-    if (id == null)
-      return ResponseEntity.status(HttpStatus.OK).body(null);
-    Member member = memberService.getSigninMember(id);
-    if (member == null)
-      return ResponseEntity.status(HttpStatus.OK).body(null);
-    // check session end
+    Member member = CheckSession(id);
+    if (member == null) return ResponseEntity.status(HttpStatus.OK).build();
 
     Accounts accounts = new Accounts();
     accountService.findByMemberId(member.getId()).forEach(account -> {
@@ -49,7 +119,7 @@ public class APIController {
     return ResponseEntity.ok(accounts);
   }
 
-  @GetMapping("/api/getBalance")
+  @GetMapping("api/balance")
   public ResponseEntity<Balance> getBalance(@SessionAttribute(name = "id", required = false) Long id,
       @RequestParam("accountId") Long accountId) {
 
@@ -72,6 +142,26 @@ public class APIController {
     return ResponseEntity.ok(balance);
   }
 
+  private Member CheckSession(Long id) {
+    if (id == null) return null;
+    Member member = memberService.getSigninMember(id);
+    if (member == null) return null;
+    else return member;
+  }
+}
+
+@Data
+class SignIn {
+  private String uid;
+  private String password;
+}
+
+@Data
+class SignUp {
+  private String uid;
+  private String password;
+  private String name;
+  private String email;
 }
 
 @Data
--- demo/src/main/java/com/example/demo/controller/SigninController.java
+++ demo/src/main/java/com/example/demo/controller/SigninController.java
@@ -1,9 +1,5 @@
 package com.example.demo.controller;
 
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,8 +7,6 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.SessionAttribute;
 
 import com.example.demo.domain.Member;
 import com.example.demo.service.MemberService;
@@ -68,135 +62,6 @@ public class SigninController {
 
     return "redirect:/home";
   }
-
-  @PostMapping("api/signin")
-  public ResponseEntity<String> apiSignin(@RequestBody Login login, BindingResult bindingResult,
-      HttpServletRequest httpServletRequest, Model model) {
-    model.addAttribute("loginType", "session-login");
-    model.addAttribute("pageName", "세션 로그인");
-
-    if (bindingResult.hasErrors()) {
-      return ResponseEntity.status(HttpStatus.FORBIDDEN).body("bindingResult.hasErrors()");
-    }
-
-    // Map<String, String> body = bodyToMap(requestBody.toString());
-
-    // String uid = body.get("uid");
-    // String password = body.get("password");
-
-    String uid = login.getUid();
-    String password = login.getPassword();
-
-    if (uid == null || uid.isEmpty() || password == null || password.isEmpty()) {
-      return ResponseEntity.status(HttpStatus.FORBIDDEN)
-          .body("uid == null || uid.isEmpty() || password == null || password.isEmpty()");
-    }
-
-    Member member = memberService.signin(uid, password);
-
-    if (member == null) {
-      return ResponseEntity.status(HttpStatus.FORBIDDEN).body("member == null");
-    }
-
-    // 로그인 성공 => 세션 생성
-
-    // 세션을 생성하기 전에 기존의 세션 파기
-    httpServletRequest.getSession().invalidate();
-    HttpSession session = httpServletRequest.getSession(true); // Session이 없으면 생성
-    // 세션에 userId를 넣어줌
-    session.setAttribute("id", member.getId());
-    session.setMaxInactiveInterval(1800); // Session이 30분동안 유지
-    
-
-    // return ResponseEntity.ok("uid="+member.getId());
-    final HttpHeaders httpHeaders = new HttpHeaders();
-    httpHeaders.setContentType(MediaType.APPLICATION_JSON);
-    return new ResponseEntity<>("{'sessionId':'" + session.getAttribute("id") + "'}", httpHeaders, HttpStatus.OK);
-  }
-
-  @PostMapping("api/signup")
-  public ResponseEntity<String> apiSignup(@RequestBody Signup signup, BindingResult bindingResult,
-      HttpServletRequest httpServletRequest) {
-
-    if (bindingResult.hasErrors()) {
-      return ResponseEntity.status(HttpStatus.FORBIDDEN).body("bindingResult.hasErrors()");
-    }
-
-    // Map<String, String> body = bodyToMap(requestBody.toString());
-
-    // String uid = body.get("uid");
-    // String password = body.get("password");
-
-    String uid = signup.getUid();
-    String password = signup.getPassword();
-    String name = signup.getName();
-    String email = signup.getEmail();
-
-    String emailName = email.split("@")[0];
-    String emailDomain = email.split("@")[1];
-
-    if (uid == null || uid.isEmpty() || password == null || password.isEmpty() || name == null || name.isEmpty()
-        || email == null || email.isEmpty()) {
-      return ResponseEntity.status(HttpStatus.FORBIDDEN)
-          .body("uid == null || uid.isEmpty() || password == null || password.isEmpty() || name == null || name.isEmpty() || email == null || email.isEmpty()");
-    }
-
-    if (emailName == null || emailDomain == null) {
-      return ResponseEntity.status(HttpStatus.FORBIDDEN).body("emailName == null || emailDomain == null");
-    }
-
-    
-    Member member = memberService.signup(uid, password, name, emailName, emailDomain);
-
-    if (member == null) {
-      return ResponseEntity.status(HttpStatus.FORBIDDEN).body("member == null");
-    }
-
-    // return ResponseEntity.ok("uid="+member.getId());
-    return new ResponseEntity<String>("success", HttpStatus.OK);
-  }
-
-  @PostMapping("api/checkSession")
-  public ResponseEntity<String> postMethodName(@RequestBody String bodyString,
-      @SessionAttribute(name = "id", required = false) Long id/*, HttpServletRequest httpServletRequest*/) {
-    
-    // HttpSession session = httpServletRequest.getSession(false); // Session이 없으면 null return
-    
-    if (id == null) {
-      return ResponseEntity.status(HttpStatus.OK).body("fail_session_null");
-    }
-    // Long sessionId = session.getAttribute("id") == null ? null : (Long) session.getAttribute("id");
-    // if (sessionId == null) {
-    //   return ResponseEntity.status(HttpStatus.OK).body("fail_session_null");
-    // }
-    Member member = memberService.getSigninMember(id);
-    if (member == null) {
-      return ResponseEntity.status(HttpStatus.OK).body("fail_member_null");
-    }
-
-    return ResponseEntity.status(HttpStatus.OK).body("success");
-  }
-
-  @PostMapping("api/signout")
-  public ResponseEntity<String> signout(HttpServletRequest request) {
-    HttpSession session = request.getSession(false); // Session이 없으면 null return
-    if (session != null) {
-      session.invalidate();
-    }
-    return ResponseEntity.status(HttpStatus.OK).body("success");
-  }
-
-  // public Map<String, String> bodyToMap(String bodyStr) {
-  // Map<String, String> body = new HashMap<>();
-  // String[] values = bodyStr.split("&");
-  // for (String value : values) {
-  // String[] pair = value.split("=");
-  // if (pair.length == 2) {
-  // body.put(pair[0], pair[1]);
-  // }
-  // }
-  // return body;
-  // }
 }
 
 class LoginRequest {
@@ -216,65 +81,5 @@ class LoginRequest {
 
   public String getPassword() {
     return password;
-  }
-}
-
-class Login {
-  private String uid;
-  private String password;
-
-  public String getUid() {
-    return uid;
-  }
-
-  public void setUid(String uid) {
-    this.uid = uid;
-  }
-
-  public String getPassword() {
-    return password;
-  }
-
-  public void setPassword(String password) {
-    this.password = password;
-  }
-}
-
-class Signup {
-  private String uid;
-  private String password;
-  private String name;
-  private String email;
-
-  public String getUid() {
-    return uid;
-  }
-
-  public void setUid(String uid) {
-    this.uid = uid;
-  }
-
-  public String getPassword() {
-    return password;
-  }
-
-  public void setPassword(String password) {
-    this.password = password;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public String getEmail() {
-    return email;
-  }
-
-  public void setEmail(String email) {
-    this.email = email;
   }
 }
